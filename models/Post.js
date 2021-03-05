fbf0c92c64658cacf8428ff2486ec21597c82ff4
quebonafide <mheimrath1994@gmail.com>
2021-03-05 15:29:45 +0100

    good luck

--- models/Post.js
+++ models/Post.js
@@ -1,20 +1,21 @@
 const { Schema, model } = require("mongoose");
 
+const postSchema = new Schema(
+  {
+    imgURL: {
+      type: String,
+      required: true,
+    },
 
-const postSchema = new Schema({
-  
-  imgURL: {
-    type: String,
-    required: true
-  },
+    description: String,
 
-  description: String,
-  
-  postedBy: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
+    postedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
     }
-});
+  },
+  { timestamps: true }
+);
 
 const Post = model("Post", postSchema);
 
--- routes/posts.js
+++ routes/posts.js
@@ -3,11 +3,11 @@ const loginCheck = require('../middleware/loginCheck')
 const Post = require('../models/Post')
 
 router.post("/postIt", loginCheck(), (req, res) => {
-  const {title, description, imgURL} = req.body
-  if(!title || !description || !imgURL){
+  const {description, imgURL} = req.body
+  if(!description || !imgURL){
     res.status(400).json({errorMessage: 'Please make something pretty for us to look at'})
   }
-  Post.create({title: title, description: description, imgURL: imgURL, postedBy: req.user})
+  Post.create({description: description, imgURL: imgURL, postedBy: req.user})
   .then(createdPost=>{res.status(200).json(createdPost)})
   .catch(err=>{console.log(err)})
 });
