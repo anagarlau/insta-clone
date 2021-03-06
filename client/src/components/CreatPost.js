3d2a09263a31c7bd507ebe54d99a5f9d2bc2ade5
Ana-Maria Garlau <garlauanamaria@gmail.com>
2021-03-06 12:46:08 +0100

    button disabled until cloudinary uploads link

--- client/src/components/CreatPost.js
+++ client/src/components/CreatPost.js
@@ -1,63 +1,56 @@
 import React from "react";
 import axios from "axios";
-import service from '../api/service'
-
-
+import service from "../api/service";
 class CreatePost extends React.Component {
   state = {
-    description: '',
-    imgURL: ''
+    description: "",
+    imgURL: "",
+    isFileUpoading: false,
   };
- 
-   
-
-  handleChange = e => {
+  handleChange = (e) => {
     const { name, value } = e.target;
     this.setState({ [name]: value });
   };
 
-  
-  handleFileUpload = e => {
+  handleFileUpload = (e) => {
     e.preventDefault();
-    console.log('The file to be uploaded is: ', e.target.files[0]);
- 
+    this.setState({ isFileUpoading: true });
+    console.log("The file to be uploaded is: ", e.target.files[0]);
     const uploadData = new FormData();
     // imageUrl => this name has to be the same as in the model since we pass
     // req.body to .create() method when creating a new thing in '/api/things/create' POST route
-    uploadData.append('imgURL', e.target.files[0]);
- 
+    uploadData.append("imgURL", e.target.files[0]);
     service
       .handleUpload(uploadData)
-      .then(response => {
+      .then((response) => {
         // console.log('response is: ', response);
         // after the console.log we can see that response carries 'secure_url' which we can use to update the state
         this.setState({ imgURL: response.secure_url });
       })
-      .catch(err => {
-        console.log('Error while uploading the file: ', err);
+      .catch((err) => {
+        console.log("Error while uploading the file: ", err);
+      })
+      .finally(() => {
+        this.setState({ isFileUpoading: false });
       });
   };
- 
-  handleSubmit = e => {
+  
+  handleSubmit = (e) => {
     e.preventDefault();
- 
     service
       .createPost(this.state)
-      .then(res => {
-        console.log('added: ', res);
+      .then((res) => {
+        console.log("added: ", res);
         // here you would redirect to some other page
-        this.props.history.push('/userprofile');
+        this.props.history.push("/userprofile");
       })
-      .catch(err => {
-        console.log('Error while adding the thing: ', err);
+      .catch((err) => {
+        console.log("Error while adding the thing: ", err);
       });
   };
-   
-  
   render() {
     console.log("this is the state", this.state);
-    console.log(this.state)
-  
+    console.log(this.state);
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
@@ -68,13 +61,13 @@ class CreatePost extends React.Component {
             onChange={this.handleChange}
           />
         </label>
-        <label>   Img:   </label>
-          <input type="file" onChange={e => this.handleFileUpload(e)} />
-     
-        <button type="submit">Post</button>
+        <label> Img: </label>
+        <input type="file" onChange={(e) => this.handleFileUpload(e)} />
+        <button disabled={this.state.isFileUpoading} type="submit">
+          Post
+        </button>
       </form>
     );
   }
 }
-
 export default CreatePost;
