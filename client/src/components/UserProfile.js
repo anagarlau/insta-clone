c52e347039ff7da525d46d963f155478b4474e87
quebonafide <mheimrath1994@gmail.com>
2021-03-09 14:18:08 +0100

    profile img

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,7 +1,8 @@
 import React from "react";
 import axios from "axios";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import service from "../api/service";
+import "../App.css";
 
 class UserProfile extends React.Component {
   constructor(props) {
@@ -18,7 +19,6 @@ class UserProfile extends React.Component {
     axios
       .get("/api/posts/userPosts")
       .then((response) => {
-       
         this.setState({ posts: response.data });
       })
       .catch((err) => {
@@ -34,29 +34,34 @@ class UserProfile extends React.Component {
     if (this.state.posts.length === 0) return <h3> Loading... </h3>;
     // console.log(this.props)
     return (
-      <div className='wall'>
-        <div> 
-       {/* <form> 
-         <img alt='imageuser' style={{width: '30px'}} src={this.props.user.imgURL} /> 
-         <input  type="file" /> 
-        </form>  */}
+      <div className="wall">
+        <div>
+          <form className="userImgForm">
+            <label for="file-input">
+              <img
+                alt="imageuser"
+                style={{ width: "30px" }}
+                src={this.props.user.imgURL}
+              />
+            </label>
+            <input id="file-input" type="file" />
+          </form>
         </div>
 
-        {this.state.posts.slice(0).reverse().map((post) => (
-          <div key={post._id} className="post" style={{ width: "18rem" }}>
-            <div className="card-body">
-              <h5 className="card-title"> {post.postedBy.username}</h5>
-              <Link to={`/allPosts/${post._id}`}>
-              <img
-                src={post.imgURL}
-                className="card-img-top"
-                alt="..."
-              />
-              </Link>
-              <p className="card-text">{post.description}</p>
+        {this.state.posts
+          .slice(0)
+          .reverse()
+          .map((post) => (
+            <div key={post._id} className="post" style={{ width: "18rem" }}>
+              <div className="card-body">
+                <h5 className="card-title"> {post.postedBy.username}</h5>
+                <Link to={`/allPosts/${post._id}`}>
+                  <img src={post.imgURL} className="card-img-top" alt="..." />
+                </Link>
+                <p className="card-text">{post.description}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     );
   }
